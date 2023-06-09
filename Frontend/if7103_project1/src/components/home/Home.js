91fe75b292efe0f5675ed94b597ccd2e145a7868
DESKTOP-72Q9BEE <yuliana.mv99@hotmail.com>
2023-06-09 17:31:59 -0600

    Carrusel

--- Frontend/if7103_project1/src/components/home/Home.js
+++ Frontend/if7103_project1/src/components/home/Home.js
@@ -1,22 +1,109 @@
 import { Carousel } from "react-bootstrap";
 import logo from "../../img/logo.svg";
+import img1 from "../../img/1.png";
+import img2 from "../../img/2.png";
+import img3 from "../../img/3.png";
+import img4 from "../../img/4.png";
+import img5 from "../../img/5.png";
+import img6 from "../../img/6.png";
+import img7 from "../../img/7.png";
+import img8 from "../../img/8.png";
+import img9 from "../../img/9.png";
+import img10 from "../../img/10.png";
+import img11 from "../../img/11.png";
+import img12 from "../../img/12.png";
 
-export function Home ({ user , setUser }) {
-    return(
+
+export function Home({ user, setUser }) {
+    return (
         <>
-            <h1>Hola {user}, en este test conocerás qué tipo de inteligencia tienes.</h1> 
-            <Carousel>
+            <h1 style={{ textShadow: '2px 2px 4px #000000'}}>Hola {user}, en este test conocerás qué tipo de inteligencia tienes.</h1>
+
+            <Carousel interval={2000} >
                 <Carousel.Item>
                     <img
-                    className="d-block w-100"
-                    src={logo}
-                    alt="First slide"
+                        className="d-block w-100"
+                        src={img1}
+                        alt="First slide"
                     />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>    
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img3}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img4}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img5}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img6}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img7}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img8}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img9}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img10}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img11}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img12}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
             </Carousel>
         </>
     )
