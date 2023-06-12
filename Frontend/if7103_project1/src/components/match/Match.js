30371be2debe3f2efa865c98b271a534f6585277
DESKTOP-72Q9BEE <yuliana.mv99@hotmail.com>
2023-06-12 17:08:34 -0600

    Agregar tipo de inteligencia en pantalla

--- Frontend/if7103_project1/src/components/match/Match.js
+++ Frontend/if7103_project1/src/components/match/Match.js
@@ -2,19 +2,23 @@ import { useEffect, useState } from "react";
 import "./Match.css"
 import Table from 'react-bootstrap/Table';
 
-export function Match ({ idUser }) {
+export function Match({ idUser }) {
     const [showMessage, setShowMessage] = useState(false)
     const [data, setData] = useState([]);
+
+    const [showMessage2, setShowMessage2] = useState(false)
+    const [data2, setData2] = useState([]);
+
     useEffect(() => {
         fetchData();
     }, []);
     const fetchData = async () => {
         try {
-            const response = await fetch('http://localhost:5000/api/users/similar/'+ idUser);
-            
-            if(!response.ok){
+            const response = await fetch('http://localhost:5000/api/users/similar/' + idUser);
+
+            if (!response.ok) {
                 setShowMessage(true)
-            }else{
+            } else {
                 const jsonData = await response.json();
                 setData(jsonData);
             }
@@ -24,29 +28,53 @@ export function Match ({ idUser }) {
         }
     };
 
-    return(
+    useEffect(() => {
+        fetchData2();
+    }, []);
+    const fetchData2 = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/users/id/' + idUser);
+
+            if (!response.ok) {
+                setShowMessage2(true)
+            } else {
+                const jsonData = await response.json();
+                setData2(jsonData);
+            }
+        } catch (error) {
+            setShowMessage2(true)
+            console.error(error);
+        }
+    };
+
+    return (
         <>
-            <h1>Personas afines con tu inteligencia</h1>
+            <div>
+                <p>Tu inteligencia actual es: <strong><em>{data2.user_intelligence}</em></strong></p>
+            </div>
+            <h2>Personas afines contigo basado en sus repuestas</h2>
+
             {showMessage && (
                 <div className="alert alert-danger" id="myMessage">
                     Debes realizar la Prueba de inteligencia para ver a las personas afines contigo.
                 </div>
             )}
+
             <Table responsive>
                 <thead>
                     <tr>
-                    <th>User</th>
-                    <th>Intelligence</th>
-                    <th>Distance</th>
+                        <th>Usuario</th>
+                        <th>Inteligencia</th>
+                        <th>Distancia</th>
                     </tr>
                 </thead>
                 <tbody>
                     {data.map((item, index) => (
-                    <tr key={index}>
-                        <td>{item.user}</td>
-                        <td>{item.intelligence}</td>
-                        <td>{item.distance}</td>
-                    </tr>
+                        <tr key={index}>
+                            <td>{item.user}</td>
+                            <td>{item.intelligence}</td>
+                            <td>{item.distance}</td>
+                        </tr>
                     ))}
                 </tbody>
             </Table>
