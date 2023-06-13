aa85ea22e272b4eebc36d356c64cd49b70376e5d
YerlinLeal <yerlin.leal@ucr.ac.cr>
2023-06-12 20:36:04 -0600

    Actualización del carrucel

--- Frontend/if7103_project1/src/components/home/Home.js
+++ Frontend/if7103_project1/src/components/home/Home.js
@@ -1,20 +1,7 @@
 import "./Home.css"
+import arrayIntelligences from "./intelligences.json"
 import { Carousel } from "react-bootstrap";
 import React from "react";
-import img1 from "../../img/1.png";
-import img2 from "../../img/2.png";
-import img3 from "../../img/3.png";
-import img4 from "../../img/4.png";
-import img5 from "../../img/5.png";
-import img6 from "../../img/6.png";
-import img7 from "../../img/7.png";
-import img8 from "../../img/8.png";
-import img9 from "../../img/9.png";
-import img10 from "../../img/10.png";
-import img11 from "../../img/11.png";
-import img12 from "../../img/12.png";
-
-const imagePaths = {img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12};
 
 export function Home({ user, setUser }) {
     return (
@@ -23,11 +10,16 @@ export function Home({ user, setUser }) {
                 Hola {user}, en este test conocerás qué tipo de inteligencia tienes.
             </h1>
             <Carousel interval={2000}>
-                {Object.values(imagePaths).map((image, index) => (
-                <Carousel.Item key={index}>
-                    <img className="d-block w-100" src={image} alt={`Slide ${index + 1}`} />
-                </Carousel.Item>
-                ))}
+            {arrayIntelligences.inteligencias.map((intelligence, index) => {
+                return (
+                    <Carousel.Item key={index}>
+                        <img className="d-block w-100" src={intelligence.url} alt={`Slide ${index + 1}`} />
+                        <Carousel.Caption>
+                            <h4 className="carousel-text">{intelligence.descripcion}</h4>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                );
+            })}
             </Carousel>
         </>
     );
